0ec4e6a1fdc34e04c674e9b94e1e9fe34484f50c
Kander Akinci <k.akinci@bkgl.bgl.schule>
2021-11-19 16:35:21 +0100

    Et Menü und Submenü eingesetzt.

--- ETFragBFTKlassenBibliothek/ETMenue.cs
+++ ETFragBFTKlassenBibliothek/ETMenue.cs
@@ -1,85 +1,86 @@
 ﻿using System;
 using Figgle;
 
-
-namespace FragBFTKlassenBibliothek
+namespace ETFragBFTKlassenBibliothek
 {
-    public class Hauptmenue
+    public class ETMenue
     {
-        public static void HauptmenueAufruf()
+
+        public static void ETSubMenue()
         {
 
 
+            string HauptAusw;
+
+ 
+            bool Hmenue;
+
             do
             {
-
                 (int, int) cPosBM = Console.GetCursorPosition();
 
                 Console.WriteLine();
 
                 //ASCII art Logo wird erzeugt.
                 Console.WriteLine
-                    (FiggleFonts.Slant.Render("FragBFT12"));
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
 
                 //Konsolentitel wird geändert.
-                Console.Title = "FragBFT12";
+                Console.Title = "BFTMultiTool";
 
                 Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                                  "                              >>> Hauptmenü <<<\n" +
+                                  "                              >>> Elektrotechnik Submenü <<<\n" +
                                   "------------------------------------------------------------------------------------\n\n");
 
-                Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");
+                Console.WriteLine("Eingabe: exit\t\t->\tbeendet das Programm");
+                Console.WriteLine("Eingabe: hauptmenü\t->\tzurück zum Hauptmenü");
 
                 //Beschreibung der Software.
-                Console.WriteLine("\n\nMit der Software 'FragBFT12' sollen wiederkehrende oder besonders aufwendige\n" +
-                                  "Aufgabenaus dem schulischen Kontext erleichtert oder gelöst werden. Diese Aufgaben\n" +
-                                  "ergeben sich aus den Problemstellungen aus dem Unterricht der Berufsfachschule für Technik.\n\n");
+                Console.WriteLine("\n\nBeschreibung Bereich \n\n");
 
-                string HauptAusw;
-
-
-
+                Hmenue = false;
 
                 //Eingabeaufforderung 
                 Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
-                Console.WriteLine("\te - Elektrotechnik");
-                Console.WriteLine("\ti - Informatik");
-                Console.WriteLine("\tm - Mathematik");
-                Console.WriteLine("\tp - Physik");
-                Console.WriteLine("\tw - Wirtschaft\n");
+                Console.WriteLine("\t1 - Feature1");
+                Console.WriteLine("\t2 - Feature2");
+                Console.WriteLine("\t3 - Feature3");
+                Console.WriteLine("\t4 - Feature4\n");
                 Console.Write("Eingabe:");
-                HauptAusw = Console.ReadLine().ToLower();
+                HauptAusw = Console.ReadLine();
 
                 switch (HauptAusw)
                 {
-                    case "e":
+
+                    case "1":
                         Console.Clear();
-                        ETFragBFTKlassenBibliothek.ETMenue.ETSubMenue();
+                        Feature1.Feature1Funcion1();
+
                         break;
 
-                    case "i":
+                    case "2":
                         Console.Clear();
-                        //ITMultiToolKlassenBibliothek.ITMenue.ITSubMenue();
+
                         break;
 
-                    case "m":
+                    case "3":
                         Console.Clear();
-                        //MAMultiToolKlassenBibliothek.MAMenue.MASubMenue();
+
                         break;
 
-                    case "p":
+                    case "4":
                         Console.Clear();
-                        //PHMultiToolKlassenBibliothek.PHMenue.PHSubMenue();
+
                         break;
 
-                    case "w":
+                    case "hauptmenü":
                         Console.Clear();
-                        //WIMultiToolKlassenBibliothek.WIMenue.WISubMenue();
+                        Hmenue = true;
                         break;
-
                     case "exit":
                         Environment.Exit(0);
                         break;
+
                     default:
 
                         Console.WriteLine("Ungültige Eingabe");
@@ -87,15 +88,14 @@ namespace FragBFTKlassenBibliothek
 
                         (int, int) cPosAM = Console.GetCursorPosition();
 
+
                         KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
 
                         break;
                 }
 
-            } while (true);
 
-
-
+            } while ( !Hmenue);
         }
     }
 }
---
+++ ETFragBFTKlassenBibliothek/Feature1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETFragBFTKlassenBibliothek
+{
+    public class Feature1
+    {
+        public static void Feature1Funcion1()
+        {
+            Console.WriteLine("Feature1");
+        }
+    }
+}
