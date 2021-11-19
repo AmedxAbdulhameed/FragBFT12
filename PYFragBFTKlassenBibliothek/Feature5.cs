4b85df65cd22fb3e9189326fe791417e2bcf305d
Kander Akinci <k.akinci@bkgl.bgl.schule>
2021-11-19 16:46:26 +0100

    Physik Submenü erstellt.

---
+++ PYFragBFTKlassenBibliothek/Feature5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PYFragBFTKlassenBibliothek
+{
+    class Feature5
+    {
+        internal static void Feature5Funcion1()
+        {
+            Console.WriteLine("Feature5");
+        }
+    }
+}
