2253466e630c7e951e5424ebbde8e82ed2561481
AngeloGrabner <angelo.grabner@gmx.de>
2021-11-24 16:48:09 +0100

    Feature zahlen geändert, kommentar mit wichtigen informationen hinzugefügt

---
+++ ITFragBFTKlassenBibliothek/Feature13(andi).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITFragBFTKlassenBibliothek
+{
+ /*
+ WICHTIG!!! 
+
+ Bitte erstelle eine neue Branch basierend auf ITEntwickellung und bearbeite da diese datei. 
+ Bitte keine Änderungen an anderen dateien vornehmen.
+ Wenn du möchtest das ich (Angelo) das menü für dein Feature erstelle, dann lass es mich wissen.
+ Wenn du mir nichts Mitteilst gehe ich davon aus das du das Menü selber erstellst.
+ Der Einstiegspunkt für deine Klasse ist die FeatureMenue Methode.
+ */
+    class Feature13
+    {
+        internal static void FeatureMenue()
+        {
+
+        }
+        internal void Feature13Funcion1()
+        {
+            Console.WriteLine("Feature");
+        }
+    }
+}
