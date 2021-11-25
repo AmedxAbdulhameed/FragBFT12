a0b3480f6644c8f39862716c702a03edb5513758
Ayyoub Bouslah <ayyoub.b2@gso.schule.koeln>
2021-11-25 12:23:51 +0100

    design verbessert und fetarue hinzugefügt ändert denn namne für euren feature

--- PYFragBFTKlassenBibliothek/PyMenue.cs
+++ PYFragBFTKlassenBibliothek/PyMenue.cs
@@ -29,23 +29,23 @@ namespace PYFragBFTKlassenBibliothek
                 Console.Title = "FragBFT";
 
                 Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                                  "                              >>> Bereich Submenü <<<\n" +
+                                  "                              >>> Bereich Pyhsik <<<\n" +
                                   "------------------------------------------------------------------------------------\n\n");
 
                 Console.WriteLine("Eingabe: exit\t\t->\tbeendet das Programm");
                 Console.WriteLine("Eingabe: hauptmenü\t->\tzurück zum Hauptmenü");
 
                 //Beschreibung der Software.
-                Console.WriteLine("\n\nBeschreibung Bereich \n\n");
+                Console.WriteLine("\n\n Hierkönnen sie bestimmte Themen von Pyhsik erarbeiten \n\n");
 
                 Hmenue = false;
 
                 //Eingabeaufforderung 
                 Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
-                Console.WriteLine("\t1 - Feature5");
-                Console.WriteLine("\t2 - Feature6");
-                Console.WriteLine("\t3 - Feature7");
-                Console.WriteLine("\t4 - Feature8\n");
+                Console.WriteLine("\t1 - Werte umrechnen ");
+                Console.WriteLine("\t2 - Volumen Berechnung von Gemotrischen Körper");
+                Console.WriteLine("\t3 - Gewichtskraft ");
+                Console.WriteLine("\t4 - Geschwindigkeiten  \n");
                 Console.Write("Eingabe:");
                 HauptAusw = Console.ReadLine();
 
