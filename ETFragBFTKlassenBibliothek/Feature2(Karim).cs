62a410ec10ca48478ec491a3f816497e25a1512c
Karim Lachgar <karim.l3@gso.schule.koeln>
2021-12-02 12:12:41 +0100

    Ohmsches Gesetz fertig

--- ETFragBFTKlassenBibliothek/Feature2(Karim).cs
+++ ETFragBFTKlassenBibliothek/Feature2(Karim).cs
@@ -13,14 +13,13 @@ namespace ETFragBFTKlassenBibliothek
         {
             string Ausw;
             Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                                     "                              >>> Ohmisches Gesetz <<<\n" +
+                                     "                              >>> Ohmsches Gesetz <<<\n" +
                                      "------------------------------------------------------------------------------------\n\n");
 
             Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
-            Console.WriteLine("\t1 - Wiederstand");
+            Console.WriteLine("\t1 - Widerstand");
             Console.WriteLine("\t2 - Spannung");
             Console.WriteLine("\t3 - Stromstärke");
-            Console.WriteLine("\t4 - Spannungsfall\n");
             Console.Write("Eingabe:");
             Ausw = Console.ReadLine();
            
@@ -29,28 +28,65 @@ namespace ETFragBFTKlassenBibliothek
 
                 case "1":
                     Console.Clear();
-      
+
+                    Console.WriteLine("----------Widerstand----------");
+
+                    Console.WriteLine("Gib die Spannung an.");
+                    double Spannung = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Gib die Stromstärke an.");
+                    double Stromstärke = Convert.ToDouble(Console.ReadLine());
+
+                    double ergebnis = (Spannung / Stromstärke);
+                    Console.WriteLine("Der Widerstand beträgt {0}.", ergebnis);
+
+                    Console.Read();
+                    Console.Clear(); 
+                     
                     break;
 
                 case "2":
                     Console.Clear();
-                    
+
+                    Console.WriteLine("-----------Spannung----------");
+                    Console.WriteLine("Gib die Stromstärke an.");
+                    double Stromstärke2 = Convert.ToDouble(Console.ReadLine());
+
+
+                    Console.WriteLine("Gib den Widerstand an.");
+                    double Widerstand2 = Convert.ToDouble(Console.ReadLine());
+
+
+                    double ergebnis2 = (Widerstand2 * Stromstärke2);
+                    Console.WriteLine("Die Spannung beträgt {0}", ergebnis2);
+
+                    Console.Read();
+                    Console.Clear();
 
                     break;
 
                 case "3":
                     Console.Clear();
-                    
+                    Console.WriteLine("----------Stromstärke----------");
 
-                    break;
+                    Console.WriteLine("Gib den Widerstand an.");
+                    double Widerstand3 = Convert.ToDouble(Console.ReadLine());
 
-                case "4":
+
+                    Console.WriteLine("Gib die Spannung an.");
+                    double Spannung3 = Convert.ToDouble(Console.ReadLine());
+                    double ergebnis3 = (Spannung3 / Widerstand3);
+
+                    Console.WriteLine("Die Stromstärke beträgt {0}", ergebnis3);
+
+                    Console.Read();
                     Console.Clear();
-                    
+
 
                     break;
 
-               
+
+                    
 
 
 
