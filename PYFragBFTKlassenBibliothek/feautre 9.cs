428d938480f8cdf8db6995aea8d9d2263a07c324
Ayyoub Bouslah <ayyoub.b2@gso.schule.koeln>
2021-12-08 14:42:52 +0100

    romeo featrue bereit zum pogrammieren

--- PYFragBFTKlassenBibliothek/feautre 9.cs
+++ PYFragBFTKlassenBibliothek/feautre 9.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Figgle;
 
 namespace PYFragBFTKlassenBibliothek
 {
@@ -10,7 +11,109 @@ namespace PYFragBFTKlassenBibliothek
     {
       internal static void Feature9Funcion1()
         {
-            Console.WriteLine("Feature9");
+
+
+            string HauptAusw;
+
+
+            bool Hmenue;
+
+            do
+            {
+                (int, int) cPosBM = Console.GetCursorPosition();
+
+                Console.WriteLine();
+
+                //ASCII art Logo wird erzeugt.
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("FragBFT"));
+
+                //Konsolentitel wird geändert.
+                Console.Title = "FragBFT";
+
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Volumen von Körper berechen <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine("Eingabe: exit\t\t->\tbeendet das Programm");
+                Console.WriteLine("Eingabe: submenü\t->\tzurück zum Hauptmenü");
+
+
+
+                Hmenue = false;
+
+                //Eingabeaufforderung 
+                Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
+                Console.WriteLine("\t1 - Pyramide");
+                Console.WriteLine("\t2 - Kugel");
+                Console.WriteLine("\t3 - Wuerfel ");
+                
+                Console.Write("Eingabe:");
+                HauptAusw = Console.ReadLine();
+
+                switch (HauptAusw)
+                {
+
+                    case "1":
+                        Console.Clear();
+                        Pyramide();
+
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Kugel();
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        Wuerfel();
+                        break;
+
+                 
+
+                    case "submenü":
+                        Console.Clear();
+                        Hmenue = true;
+                        break;
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Ungültige Eingabe");
+                        Console.ReadKey();
+
+                        (int, int) cPosAM = Console.GetCursorPosition();
+
+
+                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
+
+                        break;
+                }
+
+
+            } while (!Hmenue);
         } 
+
+
+        static void Pyramide()
+        {
+
+        }
+
+        static void Kugel()
+        {
+
+        }
+
+        static void Wuerfel()
+        {
+
+        }
+
+
+
     }
 }
