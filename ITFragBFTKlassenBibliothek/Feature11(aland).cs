168bb3ddb10b1b85a8d55001155efa7809ae4b31
alend.i2 <alend.i2@P-0G-0B005-0390>
2021-12-08 14:45:04 +0100

    feature 11 eingefügt

--- ITFragBFTKlassenBibliothek/Feature11(aland).cs
+++ ITFragBFTKlassenBibliothek/Feature11(aland).cs
@@ -7,15 +7,15 @@ using Figgle;
 
 namespace ITFragBFTKlassenBibliothek
 {
- /*
- WICHTIG!!! 
+    /*
+    WICHTIG!!! 
 
- Bitte erstelle eine neue Branch basierend auf ITEntwickellung und bearbeite da diese datei. 
- Bitte keine Änderungen an anderen dateien vornehmen.
- Wenn du möchtest das ich (Angelo) das menü für dein Feature erstelle, dann lass es mich wissen.
- Wenn du mir nichts Mitteilst gehe ich davon aus das du das Menü selber erstellst.
- Der Einstiegspunkt für deine Klasse ist die FeatureMenue Methode.
- */
+    Bitte erstelle eine neue Branch basierend auf ITEntwickellung und bearbeite da diese datei. 
+    Bitte keine Änderungen an anderen dateien vornehmen.
+    Wenn du möchtest das ich (Angelo) das menü für dein Feature erstelle, dann lass es mich wissen.
+    Wenn du mir nichts Mitteilst gehe ich davon aus das du das Menü selber erstellst.
+    Der Einstiegspunkt für deine Klasse ist die FeatureMenue Methode.
+    */
     class Feature11
     {
         internal static void FeatureMenue()
@@ -31,7 +31,7 @@ namespace ITFragBFTKlassenBibliothek
             Console.WriteLine("------------------------------------------------------------------------------------\n" +
                               "                          >>> Bereich Binär zu Dezimal <<<\n" +
                               "------------------------------------------------------------------------------------\n\n");
-            redo:
+        redo:
             Console.WriteLine("Eingabe: hauptmenü\t->\tzurück zum Hauptmenü");
             Console.WriteLine("Eingabe: zu Dezimal\t->\tum von Binär zu Dezimal umzurechnen");
             string menueAuswahl = Console.ReadLine().ToLower();
@@ -39,10 +39,10 @@ namespace ITFragBFTKlassenBibliothek
             {
                 Feature11Funcion1();
             }
-          /*else if (menueAuswahl == "zu binär") //comming soon!
-            {
-                Feature11Funcion2();
-            }*/
+            /*else if (menueAuswahl == "zu binär") //comming soon!
+              {
+                  Feature11Funcion2();
+              }*/
             else if (menueAuswahl == "hauptmenü")
             {
                 Console.Clear();
@@ -53,9 +53,39 @@ namespace ITFragBFTKlassenBibliothek
                 goto redo;
             }
         }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
         static internal void Feature11Funcion1()
         {
-            Console.WriteLine("Feature");
+                Console.WriteLine("Geben sie hiere Binärezahl ein");
+                string input;
+                input = Console.ReadLine();
+                input = Reverse(input);
+                double Summe = 0;
+                //wenn input[0] eine '1'
+                //dann addiere zur summe
+                int I = 0;
+                while (I < input.Length)
+                {
+                    if ('1' == input[I])
+                    {
+                        Summe = Summe + Math.Pow(2, I);
+                    }
+
+
+
+                    I++;
+                }
+
+                Console.WriteLine("Ihre Dezimal zahl ist {0}", Summe);
+                Console.ReadLine();
+
         }
+
     }
 }
