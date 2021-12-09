49dd07a8ae8c6ddff6d92406f58589317ad8e33f
Mahmoud Khodrawi <mahmoud.k2@gso.schule.koeln>
2021-12-09 12:48:53 +0100

    Nullstelle Code, Funktion Ausgeben Code

--- MAFragBFTKlassenBibliothek/Feature21_22.cs
+++ MAFragBFTKlassenBibliothek/Feature21_22.cs
@@ -45,7 +45,7 @@ namespace MAFragBFTKlassenBibliothek
                 Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
                 Console.WriteLine("\t1 - Wertetabell");
                 Console.WriteLine("\t2 - Nullstellen");
-                Console.WriteLine("\t2 - Funktion_Ausgeben");
+                Console.WriteLine("\t3 - Funktion Ausgeben");
 
 
                 Console.Write("Eingabe:");
@@ -108,11 +108,47 @@ namespace MAFragBFTKlassenBibliothek
 
         static void Nullstellen()
         {
+            double c = 0; // 0
+            double d; //B
+            double f; // B
+            double e;
+            double g;
 
+
+            Console.WriteLine("Nullstelle Berechnen\n");          
+            Console.WriteLine("Geben sie m ein");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben sie b ein");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0}x + {1} = {2} ", m, b, c);
+
+
+
+            d = b - b;
+
+            Console.WriteLine("{0}x + {1} = {2} ", m, d, c);
+
+            f = c - b;
+
+            Console.WriteLine("{0}x = {1} ", m, f);
+
+            e = f / m;
+
+            Console.WriteLine("{0}x {1} ", m, e);
+
+            g = m / m;
+
+            Console.WriteLine("{0}x = {1} ", g, e);
 
         }
         static void Funktion_Ausgeben()
         {
+            Console.WriteLine("Funktionsgleichung Ausgeben\n");      
+            Console.WriteLine("Geben sie m ein");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben sie b ein");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("f(x)={0}x+{1}", m, b);
 
 
         }
