229430a39ed7aac49fa5b79f2279ff216d6c9744
Hamza Modric <hamza.m9@gso.schule.koeln>
2021-12-15 13:44:48 +0100

    Feature 16 BugFix

--- WIFragBFTKlassenBibliothek/Feature16.cs
+++ WIFragBFTKlassenBibliothek/Feature16.cs
@@ -10,7 +10,34 @@ namespace WIFragBFTKlassenBibliothek
     {
         public static void Feature16Funcion1()
         {
-            Console.WriteLine("Feature16");
+            double Eingabe = 1, Lst = 0, Dst = 0, Ntgh;
+
+            do
+            {
+                Console.WriteLine("Bitte Bruttogehalt eingeben");
+                Eingabe = Convert.ToDouble(Console.ReadLine());
+                if (Eingabe <= 11000) Lst = 0;
+                else if (Eingabe <= 18000) Lst = (Eingabe - 11000) * 0.25;
+                else if (Eingabe <= 31000) Lst = (18000 - 11000) * 0.25 + (Eingabe - 18000) * 0.35;
+                else if (Eingabe <= 60000) Lst = (18000 - 11000) * 0.25 + (31000 - 18000) * 0.35 + (Eingabe - 31000) * 0.42;
+                else if (Eingabe <= 90000) Lst = (18000 - 11000) * 0.25 + (31000 - 18000) * 0.35 + (60000 - 31000) * 0.42 + (Eingabe - 60000) * 0.48;
+                else if (Eingabe <= 1000000) Lst = (18000 - 11000) * 0.25 + (31000 - 18000) * 0.35 + (60000 - 31000) * 0.42 + (90000 - 60000) * 0.48 + (Eingabe - 90000) * 0.50;
+                else Lst = (18000 - 11000) * 0.25 + (31000 - 18000) * 0.35 + (60000 - 31000) * 0.42 + (90000 - 60000) * 0.48 + (1000000 - 90000) * 0.50 + (Eingabe - 1000000) * 0.55;
+
+                Dst = (Lst / Eingabe) * 100;
+                Ntgh = Eingabe - Lst;
+                Console.WriteLine("Lohnsteuer");
+                Console.WriteLine(Lst);
+                Console.WriteLine("Nettogehalt");
+                Console.WriteLine(Ntgh);
+                Console.WriteLine("Durchschnittsteuersatz");
+                Console.WriteLine(Dst);
+
+
+
+
+
+            } while (Eingabe > 0);
         }
     }
 }
