359096f064a1868b8f34fa810e9e6cdd18901366
bjar.rashid@gmail.com <bjar.rashid@gmail.com>
2021-12-16 10:16:06 +0100

    Programm fertig

--- WIFragBFTKlassenBibliothek/Feature18.cs
+++ WIFragBFTKlassenBibliothek/Feature18.cs
@@ -1,22 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace WIFragBFTKlassenBibliothek
+namespace Zinsrechner_von_Bjar_rashid
 {
-    class Feature18
+    class Program
     {
-        internal static void Feature18Funcion1()
-        { 
-           double Kapital;
-           double Zinssatz_pro_jarh;
-           double Anlagedauer;
+        static void Main(string[] args)
+        {
+            double StartKapital;
+            double Zinsen;
+            double Jahre;
+            double Endkapital;
 
 
-            Console.WriteLine("Feature18");
+            Console.WriteLine("Geben Sie Ihr Kapital an");
+            StartKapital = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Wie viele Jahre wollen sie ausrechenen");
+            Jahre = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie Ihre Zinsen ein");
+            Zinsen = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Die Ergebnisse für die folgenden Jahre sind");
+
+
+
+            double TempPSatz = 1 + ((double)Zinsen / 100);
+            double ErgPSatz = Math.Pow(TempPSatz, Jahre);
+
+            Endkapital = StartKapital * ErgPSatz;
+            Zinsen = Endkapital - StartKapital;
+
+            Console.WriteLine($"ihr endergebnis ist: {Endkapital:f5}");
+            Console.ReadKey();
+
+
         }
     }
 }
+
 
