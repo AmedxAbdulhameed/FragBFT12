923e304439c89c458e34461adc75e546d88415c7
Tim Gavenesch <tim.g33@gso.schule.koeln>
2021-12-23 09:48:19 +0100

    Neu

--- ETFragBFTKlassenBibliothek/Feature1(Tim).cs
+++ ETFragBFTKlassenBibliothek/Feature1(Tim).cs
@@ -9,40 +9,195 @@ namespace ETFragBFTKlassenBibliothek
     public class Feature1
     {
         public static void Feature1Funcion1()
-        
+
         {
-            
 
-            {      Console.WriteLine
-                   (FiggleFonts.Slant.Render("BFTMultiTool"));
 
+            string HauptAusw;
+
+
+            bool Hmenue;
+
+            do
+            {
+                (int, int) cPosBM = Console.GetCursorPosition();
+
+                Console.WriteLine();
+                //Test Chaufan
+                //ASCII art Logo wird erzeugt.
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+
+                //Konsolentitel wird geändert.
+                Console.Title = "BFTMultiTool";
 
                 Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                                  "                              >>> Bruttorechner <<<\n" +
+                                  "                              >>> Leistungsrechner Submenü <<<\n" +
                                   "------------------------------------------------------------------------------------\n\n");
-                //Test Akinci Entwicklerpush
-                double a; // Nettobetrag
 
-                const double b = 1.07; // MwSt 7%(1.07) wenn man die MwSt dazurechnen will
+                Console.WriteLine("Eingabe: exit\t\t->\tbeendet das Programm");
+                Console.WriteLine("Eingabe: submenü\t->\tzurück zum Submenü");
 
-                const double c = 1.19; // MwSt 19%(1.19) wenn man die MwSt dazurechen will
 
-                Console.Write("Nettobetrag in Euro : ");
- 
-                a = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Brutto mit 7% MwSt : {0}\n", a * b);
+                Hmenue = false;
 
-                Console.Write("Brutto mit 19% MwSt : {0}", a * c);
+                Console.WriteLine("Mein Leistungsrechner");
+                Console.WriteLine("---------------------\n");
+                Console.WriteLine("\t1 - Elektrische Spannung * Elektrischen Strom");
+                Console.WriteLine("\t2 - Leistung / Elektrische Spannung");
+                Console.WriteLine("\t3 - Elektrischen Strom / Leistung\n");
+                Console.WriteLine("Wählen sie eine rechnung aus");
+                HauptAusw = Console.ReadLine();
 
-                Console.ReadLine();
+                switch (HauptAusw)
+                {
+
+                    case "1":
+                        Console.Clear();
+                        SpannungStrom();
+
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        LeistungSpannung();
+
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        StromLeistung();
+
+                        break;
+
+
+
+
+
+                    case "submenü":
+                        Console.Clear();
+                        Hmenue = true;
+                        break;
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Ungültige Eingabe");
+                        Console.ReadKey();
+
+                        (int, int) cPosAM = Console.GetCursorPosition();
+
+
+                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
+
+                        break;
+                }
+
+
+            } while (!Hmenue);
+        }
+
+
+        #region Funktion1(SpannungStrom)
+        static void SpannungStrom()
+        {
+            try
+            {
+
+                Console.WriteLine("Geben sie hier eine Zahl U für die elektrische spannung ein");
+                Double zahl1 = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("Geben sie Hier eine Zweite Zahl I für den elektrischen strom ein");
+                Double zahl2 = Convert.ToDouble(Console.ReadLine());
+                Double ergebnis = 0;
+
+
+                ergebnis = zahl1 * zahl2;
+
+
+                Console.WriteLine($"das ergebnis P für Leistung ist: {ergebnis}W");
+                Console.Read();
                 Console.Clear();
-               
+
             }
-               
-           
+            catch
+            {
+                Console.WriteLine("flasche eingabe");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+        }
+        #endregion
+
+        #region Funktion2(LeistungSpannung)
+        static void LeistungSpannung()
+        {
+            try
+            {
+
+                Console.WriteLine("Geben sie hier eine Zahl P für die Leistung ein");
+                Double zahl1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Geben sie Hier eine Zweite Zahl U für die elektrische Spannung ein");
+                Double zahl2 = Convert.ToDouble(Console.ReadLine());
+                Double ergebnis = 0;
+
+                ergebnis = zahl1 / zahl2;
+
+                Console.WriteLine($"das ergebnis I für den Elektrischen Strom ist: {ergebnis}A");
+                Console.Read();
+                Console.Clear();
+            }
+            catch
+            {
+                Console.WriteLine("flasche eingabe");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        #endregion
+
+        #region Funktion3(StromLeistung)
+        static void StromLeistung()
+        {
+            try
+            {
+
+                Console.WriteLine("Geben sie hier eine Zahl I für den elektrischen Strom ein");
+                Double zahl1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Geben sie Hier eine Zweite Zahl P für die Leistung ein");
+                Double zahl2 = Convert.ToDouble(Console.ReadLine());
+                Double ergebnis = 0;
+
+
+                ergebnis = zahl1 / zahl2;
+
+                Console.WriteLine($"das ergebnis U für die Elektrische Spannung ist: {ergebnis}V");
+                Console.Read();
+                Console.Clear();
+            }
+            catch
+            {
+                Console.WriteLine("flasche eingabe");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
 
 
         }
+
+
+
+        #endregion
+
+
     }
 }
