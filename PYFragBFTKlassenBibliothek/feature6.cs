ea711a296fb78c0870968c044dabe5270eae1886
Kander Akinci <k.akinci@bkgl.bgl.schule>
2021-12-28 12:29:25 +0100

    'Merge' feature6.cs 'PYEntwicklung' branch

---
+++ PYFragBFTKlassenBibliothek/feature6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PYFragBFTKlassenBibliothek
+{
+    class Class1
+    {
+        internal static void Feature6Funcion1()
+        {
+            Console.WriteLine("Feature6");
+        }
+    }
+}
